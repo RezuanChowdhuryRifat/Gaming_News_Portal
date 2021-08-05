d49b362de720f34c25b9b21b9ac5216bb9afcef5
RezuanChowdhuryRifat <87327764+RezuanChowdhuryRifat@users.noreply.github.com>
2021-08-05 21:27:17 +0600

    Adding login and logout features

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls.conf import include
+from django.conf.urls import include, url
 from feed import urls as feed_urls
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(feed_urls, namespace = 'feed'))
+    path('', include(feed_urls, namespace = 'feed')),
+    url("",include("allauth.urls") ),
 ]
 
 if settings.DEBUG:
