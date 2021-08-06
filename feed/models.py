a706b394e647e7401c9bc8a6bd13588361d79c29
RezuanChowdhuryRifat <87327764+RezuanChowdhuryRifat@users.noreply.github.com>
2021-08-06 23:25:09 +0600

    added post date and limited post feature

--- feed/models.py
+++ feed/models.py
@@ -1,10 +1,12 @@
 from typing import Text
 from django.db import models
+from django.db.models.fields import DateField
 from sorl.thumbnail import ImageField
 
 class Post(models.Model):
     text = models.CharField(max_length = 555550,blank=False, null=False)
     image = ImageField()
+    date = DateField( null=False)
        
     def __str__(self):
         return self.text
